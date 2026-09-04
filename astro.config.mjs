4a3746b9a5f79bce541a670b04a0e79d79018e50
2208 <cbbe.danawendy.gutierrez.zo@unifranz.edu.bo>
2026-09-04 08:41:40 -0400

    config: configura site y base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://DanaWendyGutierrezZOTES.github.io',
+
+  // Reemplaza con el nombre exacto de tu repositorio en GitHub:
+  base: '/mi-portafolio',
+
   integrations: [tailwind()]
 });
